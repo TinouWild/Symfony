52a19e87a3fa17b36a2b49f77c9357adfddf9343
etienne <etienne@ubuntu-tinou.2le.net>
2019-03-19 16:40:43 +0100

    Mise en forme de l'output dans le terminal après une commande perso

--- src/Command/CreateArticleCommand.php
+++ src/Command/CreateArticleCommand.php
@@ -9,6 +9,7 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -36,6 +37,24 @@ class CreateArticleCommand extends Command
             '===============',
             '',
         ]);
+        $output->writeln([
+            '<info>Texte informatif en vert</info>',
+            '<comment>Commentaire en jaune</comment>',
+            '<question>Texte noir sur fond cyan</question>',
+            '<error>Erreur en rouge</error>',
+            '',
+        ]);
+
+        // Il est également possible de mettre en forme de manière perso de deux façons :
+        // 1 - En utilisant OutputFormatterStyle :
+        $outputStyle = new OutputFormatterStyle('red', 'yellow', ['bold', 'blink']);
+        $output->getFormatter()->setStyle('fire', $outputStyle);
+
+        $output->writeln('<fire>foo</fire>');
+
+        // 2 - En ajoutant directement dans le code :
+        // bold text with underscore
+        $output->writeln('<options=bold,underscore>foo</>');
 
         $output->writeln('Article created !');
     }
