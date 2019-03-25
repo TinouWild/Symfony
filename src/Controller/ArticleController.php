2aeb1e5aead01fe45d6cf9e8c0cbf54b40fb2318
etienne <etienne@ubuntu-tinou.2le.net>
2019-03-25 10:26:19 +0100

    Creation article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
@@ -13,5 +17,27 @@ class ArticleController extends AbstractController
     public function index()
     {
         return $this->render('admin/article/index.html.twig');
+    }
+
+    /**
+     * @Route("/admin/article/new", name="article_new")
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request, ObjectManager $manager)
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($article);
+            $manager->flush();
+            $this->addFlash('success', "Article créé !");
+            return $this->redirectToRoute('articles');
+        }
+        return $this->render('admin/article/new.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Categorie;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', CKEditorType::class)
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'expanded' => true,
+                'multiple' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
